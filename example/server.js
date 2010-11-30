f93c828ed69b37e383039d37e0a2ae1aca73c4cd
Shripad K <assortmentofsorts@gmail.com>
2010-11-30 17:38:32 +0530

    first commit

--- example/server.js
+++ example/server.js
@@ -20,6 +20,8 @@ server = http.createServer(function(req, res){
       break;
       
     case '/json.js':
+    case '/jquery.js':
+    case '/chat1.html':
     case '/chat.html':
       fs.readFile(__dirname + path, function(err, data){
         if (err) return send404(res);
@@ -40,24 +42,38 @@ send404 = function(res){
 };
 
 server.listen(8080);
-
-// socket.io, I choose you
-// simplest chat application evar
-var io = io.listen(server)
-  , buffer = [];
-  
+var publisher  = require('redis').createClient();
+var redis = require('redis');
+var redisclient = redis.createClient();
+redisclient.set('connected', 1, redis.print);
+var io = io.listen(server);
 io.on('connection', function(client){
-  client.send({ buffer: buffer });
-  client.broadcast({ announcement: client.sessionId + ' connected' });
-  
-  client.on('message', function(message){
-    var msg = { message: [client.sessionId, message] };
-    buffer.push(msg);
-    if (buffer.length > 15) buffer.shift();
-    client.broadcast(msg);
-  });
+       console.log(process.memoryUsage());
+       client.on('subscribe', function(pattern) {
+               client.subscribe(pattern);
+       });
+       client.on('publish', function(pattern, message) {
+               publisher.publish(pattern, JSON.stringify(message));
+       });
+       client.on('unsubscribe', function(pattern) {
+               if(pattern) {
+                       client.unsubscribe(pattern);
+               } else {
+                       client.unsubscribe();
+               }
+       });
+			 client.on('disconnect', function(){   
+              redisclient.decr('connected', function(e, c) {
+								sys.puts("Number still connected: " + c.toString());
+          			var msg = 'Number still connected: ' + c.toString();
+								publisher.publish('*', JSON.stringify(msg));
+              })
+			client.quitGracefully();
+			 });  
 
-  client.on('disconnect', function(){
-    client.broadcast({ announcement: client.sessionId + ' disconnected' });
-  });
+      redisclient.incr('connected', function(e, c) {
+        sys.puts("Number connected: "+c.toString());
+				var msg = 'Number connected: ' + c.toString();
+				publisher.publish('*', JSON.stringify(msg));
+      });
 });
